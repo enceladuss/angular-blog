2bf593eef6ef40eb0ecd9c560faef19451e22207
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-21 00:45:54 +0300

    Adding main page layout

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
 
