bbf1d26a5e39e32965485bb7c7e0333f6d64da84
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-22 16:14:19 +0300

    firebase setting up

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {User} from '../interfaces';
+import {FbAuthResponse, User} from '../interfaces';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {tap} from 'rxjs/operators';
 
-Injectable ();
+@Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
 
@@ -12,17 +14,21 @@ export class AuthService {
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
   }
 
   logout() {
+
   }
 
   isAuthenticated(): boolean {
-    return !!this.token
+    return !!this.token;
   }
 
-  private setToken() {
-
+  private setToken(response: FbAuthResponse) {
+    console.log(response)
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCOhPSgmLow4cPRTfbDvStM8bafmgKcAC4'
 };
