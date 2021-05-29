0842fb5f12a8ba6c38194477b78da28a3cee5818
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-29 23:11:09 +0300

    Deleting post button realization

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchStr = '';
 
   constructor(
@@ -25,13 +26,19 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     });
   }
 
+  remove(id: string): void {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
+  }
+
   ngOnDestroy(): void {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
-  }
 
-  remove(id: string): void {
-
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 }
