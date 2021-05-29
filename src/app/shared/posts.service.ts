456090bb208d1c890785fe84d9e266a5932c4e7c
Yuri Nedilsky <ur1k9801@gmail.com>
2021-05-29 22:48:35 +0300

    Post list layout

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -7,7 +7,8 @@ import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   create(post: Post): Observable<Post> {
     return this.http.post(`${environment.fbDbUrl}/posts.json`, post)
@@ -16,7 +17,19 @@ export class PostsService {
           ...post,
           id: response.name,
           date: new Date(post.date)
-        }
-    }))
+        };
+      }));
   }
+
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response:{[key: string]: any}) => {
+        return Object.keys(response).map(key => ({
+          ...response[key],
+          id: key,
+          date: new Date(response[key].date)
+        }))
+      }));
+  }
+
 }
